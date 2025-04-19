1dd53f6325494aeab2d0fb02eccc105fa0ef3504
Ronny Wang <ronnywang@gmail.com>
2025-04-19 14:28:54 +0800

    改成預算的單位

---
+++ libraries/LYAPI/Type/Unit.php
@@ -0,0 +1,45 @@
+<?php
+
+class LYAPI_Type_Unit extends LYAPI_Type
+{
+    public static function getTypeSubject()
+    {
+        return '預算單位';
+    }
+
+    public static function getFilterFieldsInfo(): array
+    {
+        return [];
+    }
+
+    public static function getFieldMap()
+    {
+        return [
+        ];
+    }
+
+    public static function aggMap()
+    {
+        return [
+        ];
+    }
+
+    public static function queryFields()
+    {
+        return [
+            '機關名稱',
+        ];
+    }
+
+
+    public static function sortFields()
+    {
+        return [
+        ];
+    }
+
+    public static function getRelations()
+    {
+        return [];
+    }
+}
